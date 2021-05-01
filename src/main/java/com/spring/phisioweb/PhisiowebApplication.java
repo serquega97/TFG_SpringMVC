501758c83a10801a58e853445a3e9d9807da24d6
Sergi Quevedo Garreta <sergiquev@hotmail.com>
2021-05-01 13:15:17 +0200

    Implemented encrypted DB connection

--- src/main/java/com/spring/phisioweb/PhisiowebApplication.java
+++ src/main/java/com/spring/phisioweb/PhisiowebApplication.java
@@ -1,12 +1,15 @@
 package com.spring.phisioweb;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableEncryptableProperties
 @SpringBootApplication
 public class PhisiowebApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("jasypt.encryptor.password", "tfgencrypt");
 		SpringApplication.run(PhisiowebApplication.class, args);
 	}
 

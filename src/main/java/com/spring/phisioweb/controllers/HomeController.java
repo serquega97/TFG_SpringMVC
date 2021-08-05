4ed0a93db490901b3df84c297cc7423bc1b76b3a
Sergi Quevedo Garreta <sergiquev@hotmail.com>
2021-08-05 09:32:34 +0200

    Added language start url app

--- src/main/java/com/spring/phisioweb/controllers/HomeController.java
+++ src/main/java/com/spring/phisioweb/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.spring.phisioweb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -9,6 +10,12 @@ public class HomeController {
     @RequestMapping("/")
     public String index() {
         
+        return "redirect:/home?lang=es";
+    }
+
+    @RequestMapping("/home")
+    public String mainPage(@RequestParam String lang) {
+
         return "index";
     }
 }

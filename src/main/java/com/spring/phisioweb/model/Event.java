f1d3bb83a6f13893b96978845e457b60944362f0
Sergi Quevedo Garreta <sergiquev@hotmail.com>
2022-04-19 21:44:36 +0200

    Fix getter's and setter's

--- src/main/java/com/spring/phisioweb/model/Event.java
+++ src/main/java/com/spring/phisioweb/model/Event.java
@@ -6,26 +6,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import lombok.Getter;
-import lombok.Setter;
-
 @Entity
-@Getter
-@Setter
 public class Event {
     
     @Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	Long id;
-	String text;
-	LocalDateTime start;
-	LocalDateTime end;
+    String text;
+    LocalDateTime start;
+    LocalDateTime end;
     String resource;
     Integer servDuration;
     LocalDateTime bookingTime;
 
-    /********************************Constructors */
+    /*****************Class constructors*************/
     public Event() {
 
     }
@@ -41,6 +35,63 @@ public class Event {
         this.end = end;
         this.resource = resource;
         this.servDuration = duration;
+        this.bookingTime = bookingTime;
+    }
+
+    /*****************Getter's & setter's*************/
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getStart() {
+        return this.start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return this.end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+    public String getResource() {
+        return this.resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public Integer getServDuration() {
+        return this.servDuration;
+    }
+
+    public void setServDuration(Integer servDuration) {
+        this.servDuration = servDuration;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return this.bookingTime;
+    }
+
+    public void setBookingTime(LocalDateTime bookingTime) {
         this.bookingTime = bookingTime;
     }
 }
--- src/main/java/com/spring/phisioweb/model/Product.java
+++ src/main/java/com/spring/phisioweb/model/Product.java
@@ -7,8 +7,6 @@ package com.spring.phisioweb.model;
 
 import java.io.Serializable;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -19,8 +17,6 @@ import javax.persistence.Id;
 
 @Data
 @Entity
-@Getter
-@Setter
 @Table(name = "Product")
 public class Product implements Serializable {
     
@@ -53,8 +49,8 @@ public class Product implements Serializable {
     private Integer product_duration;
     @Column(nullable = false)
     private String product_webname;
-    
-    //Constructors
+
+    /*****************Class constructors*************/
     public Product() {
         
     }
@@ -77,5 +73,110 @@ public class Product implements Serializable {
         this.product_type = product_type;
         this.product_duration = duration;
         this.product_webname = webname;
+    }
+
+    /*****************Getter's & setter's*************/
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProduct_name() {
+        return this.product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public Float getProduct_weight() {
+        return this.product_weight;
+    }
+
+    public void setProduct_weight(Float product_weight) {
+        this.product_weight = product_weight;
+    }
+
+    public Float getProduct_price() {
+        return this.product_price;
+    }
+
+    public void setProduct_price(Float product_price) {
+        this.product_price = product_price;
+    }
+
+    public String getProduct_type() {
+        return this.product_type;
+    }
+
+    public void setProduct_type(String product_type) {
+        this.product_type = product_type;
+    }
+
+    public String getProduct_desc1() {
+        return this.product_desc1;
+    }
+
+    public void setProduct_desc1(String product_desc1) {
+        this.product_desc1 = product_desc1;
+    }
+
+    public String getProduct_desc2() {
+        return this.product_desc2;
+    }
+
+    public void setProduct_desc2(String product_desc2) {
+        this.product_desc2 = product_desc2;
+    }
+
+    public String getProduct_desc3() {
+        return this.product_desc3;
+    }
+
+    public void setProduct_desc3(String product_desc3) {
+        this.product_desc3 = product_desc3;
+    }
+
+    public String getProduct_desc4() {
+        return this.product_desc4;
+    }
+
+    public void setProduct_desc4(String product_desc4) {
+        this.product_desc4 = product_desc4;
+    }
+
+    public String getProduct_desc5() {
+        return this.product_desc5;
+    }
+
+    public void setProduct_desc5(String product_desc5) {
+        this.product_desc5 = product_desc5;
+    }
+
+    public String getUrl_photo() {
+        return this.url_photo;
+    }
+
+    public void setUrl_photo(String url_photo) {
+        this.url_photo = url_photo;
+    }
+
+    public Integer getProduct_duration() {
+        return this.product_duration;
+    }
+
+    public void setProduct_duration(Integer product_duration) {
+        this.product_duration = product_duration;
+    }
+
+    public String getProduct_webname() {
+        return this.product_webname;
+    }
+
+    public void setProduct_webname(String product_webname) {
+        this.product_webname = product_webname;
     }
 }
